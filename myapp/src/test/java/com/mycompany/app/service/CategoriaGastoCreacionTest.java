1da48335f12783b9d821954d2539bbf3036dd657
AimarMT <aimar.moreno@opendeusto.es>
2026-04-28 11:26:58 +0200

    mini cambio (me he dejado una linea useless

--- myapp/src/test/java/com/mycompany/app/service/CategoriaGastoCreacionTest.java
+++ myapp/src/test/java/com/mycompany/app/service/CategoriaGastoCreacionTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-import java.util.Arrays;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
